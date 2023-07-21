5d76de397c10190a8ebaa786e1c59cfdca5a0baa
lnomine <laurent.nomine@gmail.com>
2023-07-21 11:10:32 +0200

    Raise wait time

--- modules/bareprep/main.tf
+++ modules/bareprep/main.tf
@@ -122,7 +122,7 @@ count = var.interface != "virtual" ? 1 : 0
 
 provisioner "local-exec" {
   command = <<EOF
-    sleep 10 ; virsh qemu-agent-command ${var.vm} '{"execute":"guest-exec", "arguments":{"path":"/bin/sh", "arg":["-c", "echo '\''auto lo eth0\niface lo inet loopback'\'' > /etc/network/interfaces"]}}'
+    sleep 20 ; virsh qemu-agent-command ${var.vm} '{"execute":"guest-exec", "arguments":{"path":"/bin/sh", "arg":["-c", "echo '\''auto lo eth0\niface lo inet loopback'\'' > /etc/network/interfaces"]}}'
     virsh qemu-agent-command ${var.vm} '{"execute":"guest-exec", "arguments":{"path":"/bin/sh", "arg":["-c", "echo '\''allow-hotplug eth0\niface eth0 inet static\naddress ${var.ip}\nnetmask 255.255.255.0\ngateway ${var.gateway}'\'' >> /etc/network/interfaces"]}}'
     virsh qemu-agent-command ${var.vm} '{"execute":"guest-exec", "arguments":{"path":"/bin/sh", "arg":["-c", "systemctl restart networking"]}}'
     virsh qemu-agent-command ${var.vm} '{"execute":"guest-exec", "arguments":{"path":"/bin/sh", "arg":["-c", "echo '\''nameserver 8.8.8.8'\'' > /etc/resolv.conf"]}}'
