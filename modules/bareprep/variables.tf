15b2cf8365e74277590bb04419e17859cebb296e
Laurent Nominé <laurent.nomine@gmail.com>
2023-10-28 14:29:59 +0200

    Move vars to variables.tf

---
+++ modules/bareprep/variables.tf
@@ -0,0 +1,17 @@
+variable ip {
+  nullable = true
+}
+
+variable gateway {
+  nullable = true
+}
+
+variable mac {
+  nullable = true
+}
+
+variable interface {}
+variable vm {}
+variable disksize {}
+variable cpu {}
+variable mem {}
